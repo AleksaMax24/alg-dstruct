61244d77ce238c309e98100383133976df0076b7
AleksaMax24 <91430312+AleksaMax24@users.noreply.github.com>
2021-11-05 13:26:08 +0300

    Update labB13.cpp

--- 4-Maksimova-B13/ConsoleApplication11/labB13.cpp
+++ 4-Maksimova-B13/ConsoleApplication11/labB13.cpp
@@ -5,155 +5,167 @@
 #include "memallocator.h"
 #pragma warning(disable: 4996)
 
+
 #define TRUE 1
 #define FALSE 0
 
-#define SIZE (sizeof(header) + sizeof(int))
-
-
 typedef struct header{
-    int size;
-    struct header* next;
-    struct header* prev;
+  struct header* next;
+  struct header* prev;
+  int size;
 } header;
 
 
-typedef struct MemSyst{
-    int totalSize;
-    header* bufBlock;
-    header* curBlock;
-} MemSyst;
+static struct{
 
-MemSyst Mem = { 0, NULL, NULL };
+  header* bufBlock;
+  header* curBlock;
+  int totalSize;
+
+} MemSyst = { (header*)NULL, (header*)NULL, 0 };
 
 
 int memgetminimumsize(void){
-    return 2 * SIZE;
+  return 2 * (sizeof(header) + sizeof(int));
 }
+
 
 int memgetblocksize(void){
-    return SIZE;
+  return sizeof(header) + sizeof(int);
 }
 
 
-int meminit(void* block, int size){
-    int minSize = memgetminimumsize();
-    int blockSize = memgetblocksize();
+int meminit(void* buf, int size){
+  int minSize = memgetminimumsize();
+  int blockSize = memgetblocksize();
 
-    if (size >= minSize && block) 
-    {
-        Mem.bufBlock = (header*)block;
-        Mem.totalSize = size;
-        Mem.bufBlock->next = (header*)((char*)block + blockSize);
-        Mem.bufBlock->prev = (header*)((char*)block + blockSize);
-        Mem.bufBlock->size = 0;
-        *(int*)(Mem.bufBlock + 1) = 0;
-        Mem.bufBlock->next->next = Mem.bufBlock;
-        Mem.bufBlock->next->prev = Mem.bufBlock;
-        Mem.bufBlock->next->size = size - minSize;
-        *(int*)((char*)block - sizeof(int) + size) = Mem.bufBlock->next->size;
-        Mem.curBlock = Mem.bufBlock->next;
-        return FALSE;
-    }
-    else
-        return TRUE;
+  if (!buf || size < minSize)
+    return FALSE;
+  else
+  {
+
+    MemSyst.bufBlock = (header*)buf;
+    MemSyst.totalSize = size;
+    MemSyst.bufBlock->next = (header*)((char*)buf + blockSize);
+    MemSyst.bufBlock->prev = (header*)((char*)buf + blockSize);
+    MemSyst.bufBlock->size = 0;
+    *(int*)(MemSyst.bufBlock + 1) = 0;
+
+    MemSyst.bufBlock->next->next = MemSyst.bufBlock;
+    MemSyst.bufBlock->next->prev = MemSyst.bufBlock;
+    MemSyst.bufBlock->next->size = size - minSize;
+    *(int*)((char*)buf + size - sizeof(int)) = MemSyst.bufBlock->next->size;
+    MemSyst.curBlock = MemSyst.bufBlock->next;
+    return TRUE;
+  }
 }
 
 void* memalloc(int size){
-    header* block;
-    header* newBlock;
-    void* buffer = NULL;
-    int blockSize = memgetblocksize();
+  header* block;
+  header* newblock;
+  void* res = NULL;
+  int blockSize = memgetblocksize();
 
-    if (Mem.bufBlock == NULL)
-        return NULL;
-    if (Mem.bufBlock) {
-        block = Mem.curBlock;
-        while (block == Mem.curBlock) 
-        {
-            if (block->size < size)
-                block = block->next;
-            else if (block->size < size + blockSize)
-            {
-                buffer = (void*)(block + 1);
-                memset(buffer, 0, block->size);
-                block->prev->next = block->next;
-                block->next->prev = block->prev;
-                Mem.curBlock = block->next;
-                block->next = NULL;
-
-                *(int*)((char*)(block + 1) + block->size) = block->size;
-
-                break;
-            }
-            else
-            {
-
-                newBlock = (header*)((char*)block + blockSize + size);
-
-                newBlock->prev = block->prev;
-                newBlock->next = block->next;
-                newBlock->next->prev = newBlock;
-                newBlock->prev->next = newBlock;
-                block->next = NULL;
-
-                newBlock->size = block->size - blockSize - size;
-                *(int*)((char*)(newBlock + 1) + newBlock->size) = newBlock->size;
-
-                block->size = size;
-                *(int*)((char*)(block + 1) + size) = size;
-
-                buffer = (void*)(block + 1);
-                memset(buffer, 0, size);
-                Mem.curBlock = newBlock;
-                break;
-            }
-        }
-    }
-    return buffer;
-}
-
-void memfree(void* point){
-    int blockSize = memgetblocksize();
-    header* block;
-    header* nextBlock;
-    header* prevBlock;
-    header* end = (header*)((char*)Mem.bufBlock + Mem.totalSize);
-    if (point != NULL && Mem.totalSize != 0 && (char*)point < (char*)Mem.bufBlock + Mem.totalSize) 
+  if (MemSyst.bufBlock) 
+  {
+    block = MemSyst.curBlock;
+    do
     {
-        block = (header*)((char*)point - sizeof(header));
-        nextBlock = (header*)((char*)point + sizeof(int) + block->size);
-        prevBlock = (header*)((char*)block - *(int*)((char*)point - blockSize) - blockSize);
-        if (nextBlock != end && nextBlock->next != NULL)
-        {
-            block->size = block->size + nextBlock->size + blockSize;
-            if (nextBlock == Mem.curBlock)
-                Mem.curBlock = nextBlock->next;
-            nextBlock->prev->next = nextBlock->next;
-            nextBlock->next->prev = nextBlock->prev;
-            *(int*)((char*)(block + 1) + block->size) = block->size;
-        }
+      if (block->size < size)
+      {
+        block = block->next;
+      }
+      else if (block->size < size + blockSize) 
+      {
 
-        if (prevBlock != Mem.bufBlock && prevBlock->next != NULL)
-        {
-            prevBlock->size = prevBlock->size + block->size + blockSize;
-            if (block == Mem.curBlock)
-                Mem.curBlock = prevBlock;
-            *(int*)((char*)(prevBlock + 1) + prevBlock->size) = prevBlock->size;
-            return;
-        }
+        res = (void*)(block + 1);
+        memset(res, 0, block->size);
+        block->prev->next = block->next;
+        block->next->prev = block->prev;
+        MemSyst.curBlock = block->next;
+        block->next = NULL;
 
-        block->next = Mem.bufBlock->next;
-        Mem.bufBlock->next->prev = block;
-        block->prev = Mem.bufBlock;
-        Mem.bufBlock->next = block;
-    }
+        *(int*)((char*)(block + 1) + block->size) = block->size;
+
+        break;
+      }
+      else
+      {
+
+        newblock = (header*)((char*)block + size + blockSize);
+
+        newblock->prev = block->prev;
+        newblock->next = block->next;
+        newblock->next->prev = newblock;
+        newblock->prev->next = newblock;
+        block->next = NULL;
+
+        newblock->size = block->size - size - blockSize;
+        *(int*)((char*)(newblock + 1) + newblock->size) = newblock->size;
+
+        block->size = size;
+        *(int*)((char*)(block + 1) + size) = size;
+
+        res = (void*)(block + 1);
+        memset(res, 0, size);
+        MemSyst.curBlock = newblock;
+        break;
+      }
+    } while (block != MemSyst.curBlock);
+  }
+
+  return res;
 }
 
+
+void memfree(void* pointer){
+  int blockSize = memgetblocksize();
+  header* block;
+  header* nextblock;
+  header* prevblock;
+  header* end = (header*)((char*)MemSyst.bufBlock + MemSyst.totalSize);
+
+  if (pointer != NULL)
+  {
+    block = (header*)((char*)pointer - sizeof(header));
+    nextblock = (header*)((char*)pointer + block->size + sizeof(int));
+    prevblock = (header*)((char*)block - blockSize - *(int*)((char*)pointer - blockSize));
+
+
+    if (nextblock != end && nextblock->next != NULL)
+    {
+      block->size += nextblock->size + blockSize;
+      if (nextblock == MemSyst.curBlock)
+        MemSyst.curBlock = nextblock->next;
+      nextblock->prev->next = nextblock->next;
+      nextblock->next->prev = nextblock->prev;
+      *(int*)((char*)(block + 1) + block->size) = block->size;
+    }
+
+    if (prevblock != MemSyst.bufBlock && prevblock->next != NULL) 
+    {
+      prevblock->size += block->size + blockSize;
+      if (block == MemSyst.curBlock)
+        MemSyst.curBlock = prevblock;
+      *(int*)((char*)(prevblock + 1) + prevblock->size) = prevblock->size;
+
+      return;
+    }
+
+    block->next = MemSyst.bufBlock->next;
+    MemSyst.bufBlock->next->prev = block;
+    block->prev = MemSyst.bufBlock;
+    MemSyst.bufBlock->next = block;
+  }
+}
 
 
 void memdone(void){
-    Mem.totalSize = 0;
-    Mem.bufBlock = NULL;
-    Mem.curBlock = NULL;
+  header* block;
+  int minSize = memgetminimumsize();
+
+  block = MemSyst.bufBlock->next;
+  if (MemSyst.totalSize != block->size + minSize) {
+    fprintf(stderr, "Error\n");
+  }
 }
